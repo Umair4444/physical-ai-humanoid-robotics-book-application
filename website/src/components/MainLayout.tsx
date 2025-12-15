816112bc1af2cb9a33b8cf0f1160d80ce5fccacb
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-12-15 19:37:43 +0500

    navigation improved

--- website/src/components/MainLayout.tsx
+++ website/src/components/MainLayout.tsx
@@ -4,6 +4,7 @@ import { ThemeApplier } from './ThemeApplier/ThemeApplier';
 import Navigation from './Navigation/Navigation';
 import ScrollTopButton from './ScrollButton/ScrollTopButton';
 import FloatingChatbotButton from './FloatingChatbotButton/FloatingChatbotButton';
+import { useLocation } from '@docusaurus/router';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -16,11 +17,14 @@ const MainLayout: React.FC<MainLayoutProps> = ({
   title,
   description,
 }) => {
+  const location : any = useLocation()
+  console.log(location)
   return (
     <div className="flex flex-col min-h-screen">
       <ThemeApplier />
       <Navigation />
-      <main className="grow pt-20">
+  
+      <main className={location.pathname !== "/" ? `grow pt-20` : ""}>
         {title && <title>{title}</title>}
         {description && <meta name="description" content={description} />}
         {children}
