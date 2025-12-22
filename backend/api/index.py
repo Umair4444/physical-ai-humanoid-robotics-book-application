283e5c0c3ed55966579c5f91595d4224141b3d88
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-12-22 20:38:01 +0500

    fix

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,1 +1,5 @@
-from .app import handler
+from mangum import Mangum
+from vercel_app_simple import app_export as app
+
+# Create the Mangum adapter for serverless
+handler = Mangum(app, lifespan="off")
